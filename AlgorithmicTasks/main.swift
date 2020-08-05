33d00ca68b8131ec25e6320db1c5e9ec7675aa97
Tianid <tianid73@gmail.com>
2020-08-05 15:37:21 +0300

    Initial Commit

---
+++ AlgorithmicTasks/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AlgorithmicTasks
+//
+//  Created by Tianid on 05.08.2020.
+//  Copyright © 2020 Tianid. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
